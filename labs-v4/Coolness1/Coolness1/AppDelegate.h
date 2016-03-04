0fc393bf9105af9e711dfdc90fee5aa961c0e3f0
Jonathan Lehr <jonathan@aboutobjects.com>
2016-03-04 17:14:56 -0500

    Add Coolness1 project

---
+++ labs-v4/Coolness1/Coolness1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Coolness1
+//
+//  Created by Jonathan Lehr on 3/4/16.
+//  Copyright © 2016 About Objects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ labs-v4/Coolness1/Coolness1/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  Coolness1
+//
+//  Created by Jonathan Lehr on 3/4/16.
+//  Copyright © 2016 About Objects. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    CGRect screenRect = [UIScreen mainScreen].bounds;
+    self.window = [[UIWindow alloc] initWithFrame:screenRect];
+    self.window.backgroundColor = [UIColor lightGrayColor];
+    
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+
+@end
